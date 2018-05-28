50c1b97944e24e4209811b3b666b018bc44f321f
anirudh1107 <anirudhdeep.11@gmail.com>
2018-05-28 22:22:30 +0530

    added improved premission to feed

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -11,3 +11,15 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class PostOwnStatus(permissions.BasePermission):
+    """checks if user is updating his own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """checks the user to update their own status"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user.profile.id == request.user.id
